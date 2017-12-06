60aa333b040b6fa1e4c34018d38ad2b8ee711d17
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2017-12-07 11:53:23 +1300

    PEP8 clean-up

--- High_R_Bridge.py
+++ High_R_Bridge.py
@@ -6,9 +6,7 @@ DEVELOPMENT VERSION
 Created on Mon Jun 29 11:36:13 2015
 
 @author: t.lawson
-"""
 
-"""
 High_R_Bridge.py - Version 0.3
 A Python version of the high resistance bridge TestPoint application.
 This app is intended to offer the same functionality as the original
@@ -24,24 +22,23 @@ are output to.
 """
 
 import os
-
-#os.environ['GMHPATH'] = 'C:\Users\\t.lawson\Documents\Python Scripts\High_Res_Bridge\GMHdll'
-#os.environ['XLPATH'] = 'C:\Users\\t.lawson\Documents\Python Scripts\High_Res_Bridge'
-
 import wx
-#import wx.lib.inspection
 import nbpages as page
 import HighRes_events as evts
 import devices
 
 VERSION = "1.0"
 
-print 'HRBC',VERSION
+print 'HRBC', VERSION
 
-"""____________MainFrame Definition: holds the MainPanel in which the appliction runs_________"""
+"""
+MainFrame Definition: holds the MainPanel in which the appliction runs
+"""
+
+
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
-        wx.Frame.__init__(self,size=(900,500),*args, **kwargs) # ,size=(800,500)
+        wx.Frame.__init__(self, size=(900, 500), *args, **kwargs)
         self.version = VERSION
         self.ExcelPath = ""
 
@@ -54,18 +51,23 @@ class MainFrame(wx.Frame):
         MenuBar = wx.MenuBar()
         FileMenu = wx.Menu()
 
-        About = FileMenu.Append(wx.ID_ABOUT, text='&About', help='About HighResBridgeControl (HRBC)')
+        About = FileMenu.Append(wx.ID_ABOUT, text='&About',
+                                help='About HighResBridgeControl (HRBC)')
         self.Bind(wx.EVT_MENU, self.OnAbout, About)
-        
-        Open = FileMenu.Append(wx.ID_OPEN, text='&Open',help ='Open an Excel file')
+
+        Open = FileMenu.Append(wx.ID_OPEN, text='&Open',
+                               help='Open an Excel file')
         self.Bind(wx.EVT_MENU, self.OnOpen, Open)
 
-        Save = FileMenu.Append(wx.ID_SAVE, text='&Save',help ='Save data to an Excel file - this usually happens automatically during a run.')
-        self.Bind(wx.EVT_MENU, self.OnSave, Save)        
+        Save = FileMenu.Append(wx.ID_SAVE, text='&Save',
+                               help='Save data to an Excel file - this \
+                               usually happens automatically during a run.')
+        self.Bind(wx.EVT_MENU, self.OnSave, Save)
 
         FileMenu.AppendSeparator()
 
-        Quit = FileMenu.Append(wx.ID_EXIT, text='&Quit', help='Exit HighResBridge')
+        Quit = FileMenu.Append(wx.ID_EXIT, text='&Quit',
+                               help='Exit HighResBridge')
         self.Bind(wx.EVT_MENU, self.OnQuit, Quit)
 
         MenuBar.Append(FileMenu, "&File")
@@ -92,53 +94,57 @@ class MainFrame(wx.Frame):
         sizer.Add(self.NoteBook, 1, wx.EXPAND)
         self.MainPanel.SetSizer(sizer)
 
-    def UpdateStatus(self,e):
+    def UpdateStatus(self, e):
         if e.field == 'b':
-            self.sb.SetStatusText(e.msg,0)
-            self.sb.SetStatusText(e.msg,1)
+            self.sb.SetStatusText(e.msg, 0)
+            self.sb.SetStatusText(e.msg, 1)
         else:
             self.sb.SetStatusText(e.msg, e.field)
 
     def OnAbout(self, event=None):
         # A message dialog with 'OK' button. wx.OK is a standard wxWidgets ID.
-        dlg_description = "HRBC v"+VERSION+": A Python'd version of the TestPoint High Resistance Bridge program."
+        descr = "A Python'd version of the TestPoint High Resistance \
+        Bridge program."
+        dlg_description = "HRBC v"+VERSION+": "+descr
         dlg_title = "About HighResBridge"
-        dlg=wx.MessageDialog(self, dlg_description, dlg_title, wx.OK)
-        dlg.ShowModal() # Show dialog.
-        dlg.Destroy() # Destroy when done.
+        dlg = wx.MessageDialog(self, dlg_description, dlg_title, wx.OK)
+        dlg.ShowModal()  # Show dialog.
+        dlg.Destroy()  # Destroy when done.
 
-    def OnSave(self,event=None):
-        # WEDNESDAY
-        print 'Saving',self.page1.XLFile.GetValue(),'...'
+    def OnSave(self, event=None):
+        print 'Saving', self.page1.XLFile.GetValue(), '...'
         if self.ExcelPath is not None:
             self.page1.wb.save(self.page1.XLFile.GetValue())
             self.page1.log.close()
 
-
-    def OnOpen(self,event=None):
-        dlg = wx.FileDialog(self,message="Select data file", defaultDir=os.getcwd(), defaultFile="", wildcard="*",style=wx.OPEN|wx.CHANGE_DIR)
-        if dlg.ShowModal()==wx.ID_OK:
+    def OnOpen(self, event=None):
+        dlg = wx.FileDialog(self, message="Select data file",
+                            defaultDir=os.getcwd(), defaultFile="",
+                            wildcard="*", style=wx.OPEN | wx.CHANGE_DIR)
+        if dlg.ShowModal() == wx.ID_OK:
             self.ExcelPath = dlg.GetPath()
             self.directory = dlg.GetDirectory()
             print self.directory
             print self.ExcelPath
-            file_evt = evts.FilePathEvent(XLpath = self.ExcelPath, d = self.directory, v = VERSION)
-            wx.PostEvent(self.page1,file_evt)
+            file_evt = evts.FilePathEvent(XLpath=self.ExcelPath,
+                                          d=self.directory, v=VERSION)
+            wx.PostEvent(self.page1, file_evt)
         dlg.Destroy()
 
-    def CloseInstrSessions(self,event=None):
+    def CloseInstrSessions(self, event=None):
         for r in devices.ROLES_INSTR.keys():
             devices.ROLES_INSTR[r].Close()
         devices.RM.close()
-        print'Main.CloseInstrSessions(): closed VISA resource manager and GMH instruments'
+        print'Main.CloseInstrSessions():',
+        print'closed VISA resource manager and GMH instruments'
 
     def OnQuit(self, event=None):
         self.CloseInstrSessions()
         self.OnSave()
         self.Close()
-
-
 """_______________________________________________"""
+
+
 class MainApp(wx.App):
     """Class MainApp."""
     def OnInit(self):
@@ -151,5 +157,4 @@ class MainApp(wx.App):
 
 if __name__ == '__main__':
     app = MainApp(0)
-    #wx.lib.inspection.InspectionTool().Show()
     app.MainLoop()
