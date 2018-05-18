0e452fb5e283ce0485a0d59d7639cbb5019ab513
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2018-05-18 16:46:01 +1200

    PEP-8 tidy-up

--- High_R_Bridge.py
+++ High_R_Bridge.py
@@ -36,12 +36,16 @@ import devices
 
 VERSION = "1.1"
 
-print 'HRBC',VERSION
+print 'HRBC', VERSION
 
-"""____________MainFrame Definition: holds the MainPanel in which the appliction runs_________"""
+
+"""
+MainFrame Definition:
+holds the MainPanel in which the appliction runs
+"""
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
-        wx.Frame.__init__(self,size=(900,500),*args, **kwargs) # ,size=(800,500)
+        wx.Frame.__init__(self,size=(900,500),*args, **kwargs)
         self.version = VERSION
         self.ExcelPath = ""
 
@@ -56,12 +60,12 @@ class MainFrame(wx.Frame):
 
         About = FileMenu.Append(wx.ID_ABOUT, text='&About', help='About HighResBridgeControl (HRBC)')
         self.Bind(wx.EVT_MENU, self.OnAbout, About)
-        
-        Open = FileMenu.Append(wx.ID_OPEN, text='&Open',help ='Open an Excel file')
+
+        Open = FileMenu.Append(wx.ID_OPEN, text='&Open', help='Open an Excel file')
         self.Bind(wx.EVT_MENU, self.OnOpen, Open)
 
-        Save = FileMenu.Append(wx.ID_SAVE, text='&Save',help ='Save data to an Excel file - this usually happens automatically during a run.')
-        self.Bind(wx.EVT_MENU, self.OnSave, Save)        
+        Save = FileMenu.Append(wx.ID_SAVE, text='&Save', help='Save data to an Excel file - this usually happens automatically during a run.')
+        self.Bind(wx.EVT_MENU, self.OnSave, Save)
 
         FileMenu.AppendSeparator()
 
@@ -92,10 +96,10 @@ class MainFrame(wx.Frame):
         sizer.Add(self.NoteBook, 1, wx.EXPAND)
         self.MainPanel.SetSizer(sizer)
 
-    def UpdateStatus(self,e):
+    def UpdateStatus(self, e):
         if e.field == 'b':
-            self.sb.SetStatusText(e.msg,0)
-            self.sb.SetStatusText(e.msg,1)
+            self.sb.SetStatusText(e.msg, 0)
+            self.sb.SetStatusText(e.msg, 1)
         else:
             self.sb.SetStatusText(e.msg, e.field)
 
@@ -104,29 +108,31 @@ class MainFrame(wx.Frame):
         dlg_description = "HRBC v"+VERSION+": A Python'd version of the TestPoint High Resistance Bridge program."
         dlg_title = "About HighResBridge"
         dlg=wx.MessageDialog(self, dlg_description, dlg_title, wx.OK)
-        dlg.ShowModal() # Show dialog.
-        dlg.Destroy() # Destroy when done.
+        dlg.ShowModal()  # Show dialog.
+        dlg.Destroy()  # Destroy when done.
 
-    def OnSave(self,event=None):
+    def OnSave(self, event=None):
         # WEDNESDAY
-        print 'Saving',self.page1.XLFile.GetValue(),'...'
+        print 'Saving', self.page1.XLFile.GetValue(), '...'
         if self.ExcelPath is not None:
             self.page1.wb.save(self.page1.XLFile.GetValue())
             self.page1.log.close()
-    
-    
-    def OnOpen(self,event=None):
-        dlg = wx.FileDialog(self,message="Select data file", defaultDir=os.getcwd(), defaultFile="", wildcard="*",style=wx.OPEN|wx.CHANGE_DIR)
-        if dlg.ShowModal()==wx.ID_OK:
+
+    def OnOpen(self, event=None):
+        dlg = wx.FileDialog(self, message="Select data file",
+                            defaultDir=os.getcwd(), defaultFile="",
+                            wildcard="*", style=wx.OPEN | wx.CHANGE_DIR)
+        if dlg.ShowModal() == wx.ID_OK:
             self.ExcelPath = dlg.GetPath()
             self.directory = dlg.GetDirectory()
             print self.directory
             print self.ExcelPath
-            file_evt = evts.FilePathEvent(XLpath = self.ExcelPath, d = self.directory, v = VERSION)
-            wx.PostEvent(self.page1,file_evt)
+            file_evt = evts.FilePathEvent(XLpath=self.ExcelPath,
+                                          d=self.directory, v=VERSION)
+            wx.PostEvent(self.page1, file_evt)
         dlg.Destroy()
 
-    def CloseInstrSessions(self,event=None):
+    def CloseInstrSessions(self, event=None):
         for r in devices.ROLES_INSTR.keys():
             devices.ROLES_INSTR[r].Close()
         devices.RM.close()
