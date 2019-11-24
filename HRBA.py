d8bb41ddfb6565da2fc932b1b3d18bcecfa1eddc
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2019-11-25 10:23:44 +1300

    Update HRBA.py
    Fixed log10(-V) issue

--- HRBA.py
+++ HRBA.py
@@ -367,8 +367,8 @@ while Data_row <= Data_stop_row:
 #    G1_code = R_info.Vgain_codes_auto[round(V1set,1)]
 #    G1 = I_INFO[role_descr['DVM12']][G1_code]
 
-    sf2 = -int(round(math.log10(V2set)))  # round to nearest power of 10
-    sf1 = -int(round(math.log10(V1set)))
+    sf2 = -int(round(math.log10(abs(V2set))))  # round to nearest power of 10
+    sf1 = -int(round(math.log10(abs(V1set))))
     if 'AUTO' in range_mode:
         G2_code = R_info.Vgain_codes_auto[round(abs(V2set), sf2)]
         G1_code = R_info.Vgain_codes_auto[round(V1set, sf1)]
